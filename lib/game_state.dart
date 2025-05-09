eb487fa9d9715f9ad72358209bfc6d85ca4bee87
Ivan Šandrk <ivan.sandrk@gmail.com>
2025-05-09 15:53:22 +0900

    GameState copy eq hash tostr

--- lib/game_state.dart
+++ lib/game_state.dart
@@ -3,4 +3,22 @@ import 'package:flutter/material.dart';
 class GameState {
   Offset bluePos = Offset.zero;
   Offset? redPos;
+
+  GameState copy() {
+    var newState = GameState();
+    newState.bluePos = bluePos;
+    newState.redPos = redPos;
+    return newState;
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is GameState && other.bluePos == bluePos && other.redPos == redPos;
+
+  @override
+  int get hashCode => Object.hash(bluePos, redPos);
+
+  @override
+  String toString() => 'GameState($bluePos, $redPos)';
 }
